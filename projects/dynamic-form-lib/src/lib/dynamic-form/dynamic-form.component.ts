ed215f0413301b42df64e4ff017f1eb445ad2f95
Mirko Petrelli <mirko.petrelli@outlook.it>
2018-11-04 20:11:32 +0100

    0.0.2

--- projects/dynamic-form-lib/src/lib/dynamic-form/dynamic-form.component.ts
+++ projects/dynamic-form-lib/src/lib/dynamic-form/dynamic-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, ContentChildren, EventEmitter, Input, OnInit, Output, QueryList, SimpleChanges } from '@angular/core';
+import { Component, ContentChildren, EventEmitter, Input, OnChanges, Output, QueryList, SimpleChanges } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { ElementDirective } from '../directives/element.directive';
 import { FormControlService } from '../services/form-control.service';
@@ -9,7 +9,7 @@ import { FormControlService } from '../services/form-control.service';
   templateUrl: './dynamic-form.component.html',
   styleUrls: ['./dynamic-form.component.css']
 })
-export class DynamicFormComponent implements OnInit {
+export class DynamicFormComponent implements OnChanges {
 
   /**
    * Di tipo Base<any>[]
@@ -18,44 +18,34 @@ export class DynamicFormComponent implements OnInit {
   @Input() 
   public elements: any[] = [];
   
+  /**
+   * Oggetto contenitore e destinatario dei valori costruiti dalla form 
+   */
   @Input() 
   public target: any;
   @Output()
   public targetChange = new EventEmitter<any>();
 
-  @Output()
-  public onSave = new EventEmitter<FormGroup>();
-
-  @Output()
-  public formChange = new EventEmitter<any>();
-
   @ContentChildren(ElementDirective) 
   custom_element_templates : QueryList<any>;
 
-  
-  form: FormGroup;
+  // @Output('form')
+  // public onFormChange = new EventEmitter<FormGroup>();
+  _form: FormGroup;
  
   constructor(private _formSVC: FormControlService) {  }
- 
-  ngOnInit() {
-    console.debug(`Ricevuti elementi per form dinamica: `, this.elements);
-  }
 
   ngOnChanges(changes : SimpleChanges){
     if ( changes['elements']){
-      this.form = this._formSVC.toFormGroup(this.elements);
-      this.form.valueChanges.subscribe( event => this.formChange.emit(event));
+      this._form = this._formSVC.toFormGroup(this.elements);
     }
     if ( changes['target'] && !changes['target'].firstChange){
       if ( this.target.id ){
-        this.form = this._formSVC.toFormGroup(this.elements, this.target);
+        this._form = this._formSVC.toFormGroup(this.elements, this.target);
       }
     }
-  }
- 
-  onSubmit() {
-    console.debug(`OnSubmit Functions: `, this.form);
-    this.onSave.emit(this.form);
+
+    // this.onFormChange.emit(this._form);
   }
 
 }
